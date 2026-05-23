4c12f70d15d92e55ea5e51aa7ce161fcd564bc55
ShivangSrivastava <shivangsrivastava157@gmail.com>
2026-05-24 01:59:03 +0530

    chore(config):setup logging and configure logger

---
+++ main.py
@@ -0,0 +1,33 @@
+import logging
+from contextlib import asynccontextmanager
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+from api.routes import router
+from config.logging_config import setup_logging
+from db.postgres import Postgres
+
+setup_logging()
+
+logger = logging.getLogger(__name__)
+
+db = Postgres()
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    logger.info("Application starting...")
+    load_dotenv()
+    await db.connect()
+
+    yield
+
+    logger.info("Application shutting down...")
+
+    await db.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router)
