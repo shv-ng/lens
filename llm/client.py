54a4de795c839af0b8b7b7f1c291b9caa3409438
ShivangSrivastava <shivangsrivastava157@gmail.com>
2026-05-23 19:33:29 +0530

    fix(llm/client.py): add logging to client methods

--- llm/client.py
+++ llm/client.py
@@ -1,13 +1,17 @@
 from pydantic import BaseModel
 from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
 
-llm = ChatGroq(
+# llm = ChatGroq(
+#     model="llama-3.3-70b-versatile",
+# )
+llm = ChatOllama(
     model="llama-3.3-70b-versatile",
-    temperature=0,
 )
 
 
-def get_llm(schema: type[BaseModel] | None = None):
+def get_llm(schema: type[BaseModel] | None = None, temperature: int = 0):
+    llm.temperature = temperature
     if not schema:
         return llm
     return llm.with_structured_output(schema)
